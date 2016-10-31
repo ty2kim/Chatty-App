a96c736e0b8023d4114ea947b8262ec61956d9e4
TAE YOUNG KIM <ty2kim@gmail.com>
2016-10-31 22:14:34 +0000

    first commit of chatty app

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     watchOptions: {
       aggregateTimeout: 300,
-      poll: 1000
-    }
+      poll: 1000,
+    },
   })
   .listen(3000, '0.0.0.0', function (err, result) {
     if (err) {
