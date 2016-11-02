e717faf2006475d9c17007aec6576cadd4c517fd
TAE YOUNG KIM <ty2kim@gmail.com>
2016-11-02 00:20:14 +0000

    day2 done

--- src/App.jsx
+++ src/App.jsx
@@ -9,33 +9,45 @@ class App extends Component {
       currentUser: { name: 'Bob' },
       messages: [
         {
-          id: 1,
+          //id: 1,
           username: 'Bob',
           content: 'Has anyone seen my marbles?',
         },
         {
-          id: 2,
+          //id: 2,
           username: 'Anonymous',
           content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.',
         },
       ],
     };
     super(props);
+    // it's ok to do this in constructors
     this.state = data;
   }
 
+
+
+  // functions that comes with react, don't use ES6
+  // no need to bind as well because it's all done by react
+  // when we do 'extends Component'
   componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      // Get current state, and find 'messages' and update
-      this.setState({messages: messages})
-    }, 3000);
+    // setTimeout(() => {
+    //   console.log("Simulating incoming message");
+    //   //Add a new message to the list of messages in the data store
+    //   const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+    //   const messages = this.state.messages.concat(newMessage)
+    //   //Update the state of the app component.
+    //   //Calling setState will trigger a call to render() in App and all child components.
+    //   this.setState({messages: messages})
+    // }, 3000);
+  }
+
+  addMessage = (newIncomingMessage) => {
+    const newMessage = {username: this.state.currentUser.name, content: newIncomingMessage};
+    // why use concat
+    // does not change the current state until 'setState' is called
+    const updatedMessages = this.state.messages.concat(newMessage);
+    this.setState({messages: updatedMessages});
   }
 
   render() {
@@ -45,7 +57,10 @@ class App extends Component {
           <h1>Chatty</h1>
         </nav>
         <MessageList messages={this.state.messages}/>
-        <ChatBar currentUser={this.state.currentUser.name}/>
+        <ChatBar
+          currentUser={this.state.currentUser}
+          addMessage={this.addMessage}
+        />
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,6 +1,45 @@
 import React, { Component } from 'react';
 
 class ChatBar extends Component {
+
+  constructor(props) {
+    super(props);
+    // https://github.com/react-toolbox/react-toolbox/issues/652
+    // using 'bind' way
+    //this.handleChange = this.handleChange.bind(this);
+    //this.submitMessage = this.submitMessage.bind(this);
+    this.state = {message: '', username: this.props.currentUser.name};
+  }
+
+  // need to use ES6 if you don't want to use bind
+  handleChange = (e) => {
+    this.setState({message: e.target.value});
+  }
+
+  submitMessage = (e) => {
+    if (e.key === 'Enter') {
+      this.props.addMessage(e.target.value);
+      // <bad way!>
+      // this.state.message = '';
+      // this.setState(this.state);
+      // <better way>
+      const emptyMessage = '';
+      this.setState({message: emptyMessage});
+    }
+  }
+
+  // handleChangeName(e) {
+  //   this.state.username = e.target.value;
+  //   this.setState(this.state);
+  // }
+  //
+  // changeName(e) {
+  //   if (e.key === 'Enter') {
+  //     this.props.changeName(e.target.value);
+  //     this.setState(this.state);
+  //   }
+  // }
+
   render() {
     return (
       <footer>
@@ -8,12 +47,21 @@ class ChatBar extends Component {
           id="username"
           type="text"
           placeholder="Your Name (Optional)"
-          value={this.props.currentUser}
+          value={this.props.currentUser.name}
+          //onChange={this.handleChangeName}
+          //onKeyUp={this.changeName}
         />
         <input
           id="new-message"
           type="text"
           placeholder="Type a message and hit ENTER"
+          value={this.state.message}
+          //onChange={(e) => this.handleChange(e)}
+          //to not use callbacks which is bad,
+          //need to use bind(equally bad) or es6(better) when defining functions
+          //if you use es6, no need for bind or callbacks!
+          onChange={this.handleChange}
+          onKeyUp={this.submitMessage}
         />
       </footer>
     );
